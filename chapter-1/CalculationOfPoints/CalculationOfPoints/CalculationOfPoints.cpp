4cd33eabdd29471e305c083eea7d2e8e1adaaf19
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-02-15 20:48:23 +0300

    CalculationOfPoints: Add initial version

---
+++ chapter-1/CalculationOfPoints/CalculationOfPoints/CalculationOfPoints.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int a, b, c;
+    int score = 0;
+    int arithmetic_mean_number = 3;
+
+    cout << "Добро пожаловать в нашу игру по вычислению средне арифметического Ваших очков" << endl;
+
+    cout << "Введите первую сумму очков: " << endl;
+    cin >> a;
+
+    cout << "Введите первую сумму очков: " << endl;
+    cin >> b;
+
+    cout << "Введите первую сумму очков: " << endl;
+    cin >> c;
+
+    score = (a + b + c) / arithmetic_mean_number;
+
+    cout << "Средне арифметическое очков = " << score << endl;;
+}
