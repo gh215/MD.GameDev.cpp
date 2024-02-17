c2f520c044dbbc207fd9eaebfe6e1a2b56a13ff2
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-02-17 13:17:03 +0300

    WordsKneader: Added test-cases and changed the code

--- chapter-3/WordKneader/WordKneader/WordKneader.cpp
+++ chapter-3/WordKneader/WordKneader/WordKneader.cpp
@@ -5,7 +5,18 @@
 
 using namespace std;
 
-
+/* Тест-кейсы
+    1. Если пользователь угадал слово wall без использования подсказки, то он получает 4 очка
+    2. Если пользователь угадал слово glasses без использования подсказки, то он получает 7 очков
+    3. Если пользователь угадал слово labored без использования подсказки, то он получает 7 очков
+    4. Если пользователь угадал слово persistent без использования подсказки, то он получает 10 очков
+    5. Если пользователь угадал слово jumble без использования подсказки, то он получает 6 очков
+    6. Если пользователь угадал слово wall с использованием подсказки, то он получает 1 очко
+    7. Если пользователь угадал слово glasses с использованием подсказки, то он получает 4 очка
+    8. Если пользователь угадал слово labored с использованием подсказки, то он получает 4 очка
+    9. Если пользователь угадал слово persistent с использованием подсказки, то он получает 7 очков
+    10. Если пользователь угадал слово jumble с использованием подсказки, то он получает 3 очка
+*/
 int main()
 {
     setlocale(0, "Russian");
@@ -14,13 +25,11 @@ int main()
     const int NUM_WORDS = 5;
     const string WORDS[NUM_WORDS][NUM_FIELDS] =
     {
-
          {"wall", "Чувствуешь ли ты, что бьёшься головой о что-то?"},
          {"glasses", "Это поможет вам увидеть ответ на вопрос."},
          {"labored", "Слишком медленно, не так ли?"},
          {"persistent", "Продолжайте в том же духе!"},
          {"jumble", "В этом и заключается смысл игры."}
-
     };
 
     srand(static_cast<unsigned int>(time(0)));
@@ -28,7 +37,6 @@ int main()
     string theWord = WORDS[choice][WORD];
     string theHint = WORDS[choice][HINT];
     int score = 0;
-
     string jumble = theWord;
     int length = jumble.size();
 
@@ -41,10 +49,10 @@ int main()
         jumble[index2] = temp;
     }
 
-    int scoreWord = theWord.length();
     const int scoreHint = 3;
-    int score1 = score - scoreHint;
-    int score2 = score1 + scoreWord;
+    int score_used_hint = 0;
+    int score_not_used_hint = 0;
+    bool usedHint = false;
 
     cout << "\t\t Добро пожаловть в игру 'Словомеска'!\n\n";
     cout << "Соберите буквы, чтобы отгадать слово\n";
@@ -61,7 +69,9 @@ int main()
         if (guess == "hint")
         {
             cout << theHint;
-            cout << "\nВы воспользовались подсказкой. Ваш текущий результат очков = " << score1 << endl;
+            usedHint = true;
+            score_used_hint = score - scoreHint;
+            cout << "\nВы воспользовались подсказкой. Ваш текущий результат очков = " << score_used_hint << endl;
         }
         else
         {
@@ -74,7 +84,17 @@ int main()
     if (guess == theWord)
     {
         cout << "Да, всё верно!";
-        cout << "Ваш текущий результат очков = " << score2 << endl;
+        if (!usedHint)
+        {
+            score_not_used_hint += theWord.length();
+            cout << "Ваш текущий результат очков = " << score_not_used_hint << endl;
+        }
+        else
+        {
+            score_used_hint += theWord.length();
+            cout << "Ваш текущий результат очков = " << score_used_hint << endl;
+        }
+        
     }
 
     cout << "Спасибо за игру! Ждём Вас снова!";
