a8e56d1dd090871c38e7858f534fcb3c22150dc6
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-02-23 15:05:10 +0300

    GuessTheNumber: added implementation of test-cases of abnormal user: continuation of the game after wrong number set and declaring the user as a dishonest player if he changes the set number

--- chapter-2/GuessTheNumber/GuessTheNumber/GuessTheNumber.cpp
+++ chapter-2/GuessTheNumber/GuessTheNumber/GuessTheNumber.cpp
@@ -1,21 +1,24 @@
 ﻿#include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 
 
 /* Тест-кейсы для нормального пользователя
     1. П задумал число на нижней границе:
     число больше - К генерирует число меньше
-    2. 
-    3. 
-    Сужает диапазон
-    Число шагов не
-    За 10 шагов компьютер должен угадать число
+    2. П задумал число на верхней границе:
+    число меньше - К генерирует число больше
+    3. П задумал число на середине:
+    Сужает диапазон. Число меньше - ставит меньшую границу диапазона
+    Число больше - ставит большую границу диапазона
+
+    За 7 шагов компьютер должен угадать число
 
     Тест-кейсы для ненормального пользователя
-    1. За диапазоном
-    2. Недопустимы ответ (пр. 4)
-    3. Пользователь меняет показания
+    1. За диапазоном - выдаёт ошибку
+    2. Недопустимы ответ (например 4), компьютер должен продолжать игру
+    3. Пользователь меняет показания - программа называет его обманщиком
 
 */  
 
@@ -23,12 +26,20 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
+    setlocale(LC_ALL, "Russian");    
+    srand(static_cast<unsigned int>(time(0)));
+
     enum { SMALL = 1, BIG, CORRECT };
+
     int user_conceive, computerNumber;
     int tries = 0;
     int min = 1, max = 50;
-    srand(static_cast<unsigned int>(time(0)));
+
+    bool changing_numbers = false;
+
+    string small_number = "число слишком маленькое";
+    string big_number = "число слишком большое";
+
 
     cout << "Добро пожаловать в нашу игру! Вы загадываете число в уме от " << min << " до " << max << ", а компьютер его отгадывает"
         "\nВы будете писать " << SMALL << ", " << BIG << " или " << CORRECT << " после каждого числа, выведенного компьютером: \n"
@@ -38,45 +49,57 @@ int main()
     {
         if (max <= min)
         {
-            cout << "Ошибка! Загаданное число не может быть 0 или больше 50!" << endl;
+            cout << "\nОшибка! Загаданное число не может быть 0 или больше 50!" << endl;
             return 0;
         }
 
         tries++;
-        computerNumber = rand() % (max - min + 1) + min;  
-       
-        cout << "Компьютер загадал такое число: "
+
+        computerNumber = rand() % (max - min + 1) + min;     
+        cout << "\nКомпьютер загадал такое число: "
             << computerNumber
             << ". Оно больше, меньше или равно загаданному числу?"
             << endl;
-
 
         cout << "Ваш ответ: ";
         cin >> user_conceive;
 
         if (user_conceive == SMALL)
         {
-            cout << "Число слишком маленькое" << endl;
+
+            cout << "\nОтвет пользователя: " << small_number << endl;
             min = computerNumber + 1;
         }
 
         else if (user_conceive == BIG)
         {
-            cout << "Число слишком большое" << endl;
+            cout << "\nОтвет пользователя: " << big_number << endl;
             max = computerNumber - 1;
-
         }
         else if (user_conceive == CORRECT)
         {
-            cout << "Компьютер угадал ваше число и оно = " << computerNumber <<
+            cout << "\nКомпьютер угадал ваше число и оно равно " << computerNumber <<
                 " и на это ушло " << tries << " попыток" << endl;
-            return 0;
+            break;
         }
         else
         {
-            cout << "Ошибка! Неверно введённое число!" << endl;
-            return 0;
+            cout << "\nОшибка! Неверно введённое число!" << endl;
+            continue;
         }      
+        if (min > max || max < min)
+        {
+            changing_numbers = true;
+            cout << "\nТы обманываешь меня!" << endl;
+            return 0;
+        }
        
-    }   while (user_conceive != CORRECT); 
+    }   while (user_conceive != CORRECT && tries != 7); 
+
+    if (tries == 7) 
+    {
+        cout << "К сожалению, компьютеру не удалось угадать число за " << tries << " попыток.\n";
+    }
+
+    return 0;
 }
