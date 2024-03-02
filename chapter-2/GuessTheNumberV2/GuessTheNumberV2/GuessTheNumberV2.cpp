a9ca53a512c874266d0312e021a3f4826bf3a5c0
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-03-02 15:57:00 +0300

    GuessTheNumberV2: Add main program structure

---
+++ chapter-2/GuessTheNumberV2/GuessTheNumberV2/GuessTheNumberV2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include "gtn.h"
+
+using namespace std;
+void game()
+{
+    greeting();
+    int min = border_min;
+    int max = border_max;
+    int comp_num;
+    do
+    {
+        comp_num = num_gen(min, max);
+        int user_cons = ask_user(comp_num);
+        corr_check(user_cons, comp_num, min, max);
+        if (user_cons == CORRECT)
+        {
+            break;
+        }
+        bord_reduct(comp_num, user_cons, min, max);
+        if (min == max)
+        {
+            break;
+        }
+
+    } while (true);
+
+    tell_guessed(comp_num);
+}
+
+int main()
+{
+    //Init random numbers
+    game();
+}
+
---
+++ chapter-2/GuessTheNumberV2/GuessTheNumberV2/gtn.h
@@ -0,0 +1,16 @@
+#pragma once
+
+enum answers { SMALL = 1, BIG, CORRECT };
+const int border_min = 1;
+const int border_max = 50;
+
+void greeting();
+int num_gen(int min, int max);
+int ask_user(int cn);
+void corr_check(int uc, int cn,  
+	int min, int max);
+void tell_guessed(int cn);
+void bord_reduct(int cn, int uc, int &min, int &max);
+
+
+
