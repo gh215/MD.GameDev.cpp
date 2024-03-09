ec8fea1a33a65cab5fb7805bd711a4bee7f66c3f
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-03-09 20:17:49 +0300

    GuessTheNumberV2: Add test-cases

---
+++ chapter-2/GuessTheNumberV2/UnitTest/UnitTest.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "..\GuessTheNumberV2\gtn.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		TEST_METHOD(NumGenTest)
+		{
+			int ng = num_gen(1, 100);
+			Assert::IsTrue(ng >= 1 && ng <= 100);
+		}
+		TEST_METHOD(NumGenNegTest)
+		{
+			int ng = num_gen(-100, -1);
+			Assert::IsTrue(ng <= -1 && ng >= -100);
+		}
+		TEST_METHOD(NumGenZeroTest)
+		{
+			int ng = num_gen(0, 0);
+			Assert::IsTrue(ng == 0);
+		}
+	};
+}
