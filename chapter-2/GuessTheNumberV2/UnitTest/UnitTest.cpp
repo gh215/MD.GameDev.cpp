d5bd4ac2e977954da2de81815f3f6a81947ce950
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-03-14 16:17:39 +0300

    GuessTheNumberV2: Added AskUserTest, TellGuessedTest, BordReductTest, GreetingTest test-cases

--- chapter-2/GuessTheNumberV2/UnitTest/UnitTest.cpp
+++ chapter-2/GuessTheNumberV2/UnitTest/UnitTest.cpp
@@ -24,5 +24,35 @@ namespace UnitTest
 			int ng = num_gen(0, 0);
 			Assert::IsTrue(ng == 0);
 		}
+		TEST_METHOD(GreetingTest)
+		{
+			greeting();
+		}
+		TEST_METHOD(BordReductTest)
+		{
+			int min = 1, max = 100;
+			bord_reduct(50, BIG, min, max);
+			Assert::AreEqual(1, min);
+			Assert::AreEqual(49, max);
+
+			min = 1, max = 100;
+			bord_reduct(50, SMALL, min, max);
+			Assert::AreEqual(51, min);
+			Assert::AreEqual(100, max);
+		}
+		TEST_METHOD(TellGuessedTest)
+		{
+			tell_guessed(42);
+			Assert::IsTrue(42);
+		}
+		TEST_METHOD(AskUserTest)
+		{
+			int min = 1, max = 100;
+			int cn = 50;
+			int userInput = SMALL;
+			int result = ask_user(cn, min, max);
+			Assert::AreEqual(userInput, result);
+		}
+
 	};
 }
