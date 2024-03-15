071ef5496a810402963e35a9e182c00171cf4697
Yaroslav Novodvorskiy <yaroslavgh2016@gmail.com>
2024-03-15 15:48:34 +0300

    CrazyLibrarians: Add initial version

---
+++ chapter-6/CrazyLibrarians/CrazyLibrarians/CrazyLibrarians.cpp
@@ -0,0 +1,21 @@
+﻿#include "czlib.h"
+
+int main()
+{
+
+	setlocale(0, "Russian");
+
+	cout << "\nДобро пожаловать в игру 'Безумные библиотекари' ";
+	cout << "\nОтветьте на следующие вопросы, чтобы помочь создать новую историю.";
+	string name = askText("\nПожалуйста, введите имя: ");
+	string noun = askText("\nПожалуйста, укажите существительное во множественном числе: ");
+
+	int number = askNumber("\nПожалуйста, введите число: ");
+
+	string bodyPart = askText("\nПожалуйста, укажите часть тела: ");
+	string verb = askText("\nПожалуйста, введите глагол: ");
+
+	tellStory(name, noun, number, bodyPart, verb);
+
+	return 0;
+}
