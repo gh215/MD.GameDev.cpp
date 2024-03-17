e24a2f23b460fc2fb7b74558491e82b5a2fb5d96
Alexey Mavrin <amavrin@gmail.com>
2024-03-17 13:46:13 +0300

    czlib.h: add function declaration

--- chapter-6/CrazyLibrarians/CrazyLibrarians/czlib.h
+++ chapter-6/CrazyLibrarians/CrazyLibrarians/czlib.h
@@ -8,3 +8,4 @@ using namespace std;
 string askText(const char* prompt);
 int askNumber(const char* prompt);
 void tellStory(string& name, string& noun, int number, string& bodyPart, string& verb);
+void set_io_streams(istream*, ostream*);
